97911757c5dcbd97030ddfb1a5ef4209fa1e00be
qiduanduan326 <165464227+qiduanduan326@users.noreply.github.com>
2024-04-24 16:25:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+   Hello Vue ccc! 
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
